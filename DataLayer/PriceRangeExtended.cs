8bb33645c0d454f8c56ebef9c0c168ebeb854748
Mehrdad <mehrdud@gmail.com>
2014-10-01 17:10:13 +0100

    test

---
+++ DataLayer/PriceRangeExtended.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    [MetadataTypeAttribute(typeof(PriceRangeMetadata))]
+    public partial class PriceRange
+    {
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+
+
+    class PriceRangeMetadata
+    {
+
+        
+    }
+}
---
+++ DataLayer/ProvinceExtended.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    [MetadataTypeAttribute(typeof(ProvinceMetadata))]
+    public partial class Province
+    {
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+
+
+    class ProvinceMetadata
+    {
+
+        
+    }
+}
